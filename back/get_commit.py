69f5a6360a119113198e83a98e2eec32768eaf8d
jrastit <jerome.rastit@aitivity.com>
2023-09-23 09:09:50 -0400

    add add commit script

--- back/get_commit.py
+++ back/get_commit.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
 url = "https://api.github.com/search/commits?q=a36d5830147958d8d6f771815f7df07ca3672bcb"
 
 headers = {
-  'Authorization': 'Token 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'
+  'Authorization': 'Token ' + os.env['GITHUB_API_KEY']
 }
 
 response = requests.request("GET", url, headers=headers)
