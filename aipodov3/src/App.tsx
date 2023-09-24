2eb0a149590443c076ffd0ea2414c2b545e0b6d8
Xavier <xavier@localhost>
2023-09-24 02:14:41 +0200

    Update app layout

--- aipodov3/src/App.tsx
+++ aipodov3/src/App.tsx
@@ -1,10 +1,6 @@
-import './App.css'
-
 import {createWeb3Modal, defaultWagmiConfig} from '@web3modal/wagmi/react'
-
 import {WagmiConfig} from 'wagmi'
 import {goerli, mainnet, polygon, polygonMumbai, scrollSepolia, scrollTestnet} from 'wagmi/chains'
-import ConnectButton from "./ConnectButton";
 import Dataset from "./dataset/Dataset";
 
 // 1. Get projectId
@@ -20,9 +16,27 @@ createWeb3Modal({wagmiConfig, projectId, chains})
 function App() {
     return (
         <WagmiConfig config={wagmiConfig}>
-            <img src="/logo.jpg"/>
-            <ConnectButton/>
-            <Dataset/>
+            <div style={{
+                display: "flex",
+                flexDirection: 'column',
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: 'row',
+                    justifyContent: "space-between",
+                }}>
+                    <img src="/logo.jpg"/>
+                    <span style={{
+                        padding: '20px 10px'
+                    }}>
+                            <w3m-button/>
+                        </span>
+                </div>
+                <div>
+                    <Dataset/>
+                </div>
+            </div>
+
         </WagmiConfig>
     )
 }
